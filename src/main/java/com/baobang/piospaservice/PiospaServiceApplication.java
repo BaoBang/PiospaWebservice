e7fe76068b2bb5bccda9d9a721ef04f3fe7eaaa5
Nguyễn Bảo Bằng <baobangb5@gmail.com>
2018-07-03 17:15:38 +0700

    add all

---
+++ src/main/java/com/baobang/piospaservice/PiospaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.baobang.piospaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiospaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiospaServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/baobang/piospaservice/PiospaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baobang.piospaservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PiospaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
